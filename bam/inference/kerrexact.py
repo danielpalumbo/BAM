6d4f27dc4bf30619349dc08779d0ea509961b5d6
danielpalumbo <daniel.palumbo@cfa.harvard.edu>
2021-06-25 17:52:42 -0400

    WIP: kerr interp is fast but wrong :(

--- bam/inference/kerrexact.py
+++ bam/inference/kerrexact.py
@@ -7,7 +7,7 @@ import numpy as np
 import mpmath as mp
 import matplotlib.pyplot as plt
 from scipy.interpolate import RectBivariateSpline, interp1d, interp2d
-# import scipy.interpolate as si
+import scipy.interpolate as si
 
 #define elliptic functions (need the mpmath version to take complex args)
 sn = np.frompyfunc(mp.ellipfun, 3, 1)
@@ -82,124 +82,206 @@ def get_radius_exact(rho, varphi, inc, a, n):
     m = np.sign(beta)
     m[m<0] = 0
     m += n
+    print(np.arcsin(np.sqrt(r31/r41)))
     fobs = np.complex128(ef(np.arcsin(np.sqrt(r31/r41)), k))
+    plt.imshow(np.imag(fobs).reshape((40,40)))
+    plt.colorbar()
+    plt.show()
     Fobs = np.complex128(ef(np.arcsin(np.cos(inc)/np.sqrt(up)), up/um))
     Ir = np.real(1/np.sqrt(-um*a**2)*(2*m*np.complex128(ef(np.pi/2, up/um)) - np.sign(beta)*Fobs))
-    # plt.imshow(Ir)
-    # plt.title('Ir')
-    # plt.show()
+    
     ffac = 1 / 2 * (r31 * r42)**(1/2)
+    # plt.imshow(np.real(ffac*Ir-fobs))
+    # plt.colorbar()
+    # plt.show()
+    snnum = np.complex128(sn('sn',ffac*Ir-fobs,k))
+
+    # plt.imshow(np.angle(ffac*Ir-fobs).reshape((40,40)))
+    # plt.title("Angle(ffac*Ir-fobs)")
+    # plt.colorbar()
+    # plt.show()
+    # plt.imshow(np.angle(k).reshape((40,40)))
+    # plt.title('Angle(k)')
+    # plt.colorbar()
+    # plt.show()
+    # plt.imshow(np.imag(snnum).reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
+    # plt.imshow(np.imag(ffac*Ir-fobs).reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
+    # plt.imshow(np.imag(k).reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
     snsqr = np.complex128(sn('sn', ffac * Ir - fobs, k))**2
+    # plt.imshow(np.real(snsqr).reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
     rs = np.real((r4*r31 - r3*r41*snsqr) / (r31-r41*snsqr))
     return rs
 
-def build_all_interpolators(rho_interp):
-    nrho = len(rho_interp)
-    varphi_interp = np.linspace(1e-5,2*np.pi,50)
-    theta_interp = np.linspace(1e-3,np.pi/2-1e-3,50)
-    a_interp = np.linspace(1e-3,1-1e-3,50)
-    rr, vv, tt, aa = np.meshgrid(rho_interp, varphi_interp, theta_interp, a_interp)
-
-
-    # rr, vv = np.meshgrid(rho_interp, varphi_interp)
-    aa = rr*np.cos(vv)
-    bb = rr*np.sin(vv)
-    all_lam, all_eta = get_lam_eta(aa,bb,tt,aa)
-    all_up, all_um = get_up_um(all_lam, all_eta, aa)
-    all_r1, all_r2, all_r3, all_r4 = get_radroots(all_lam, all_eta, aa)
-
-    all_r32 = all_r3-all_r2
-    all_r41 = all_r4-all_r1
-    all_r31 = all_r3-all_r1
-    all_r42 = all_r4-all_r2
-    all_k = all_r32*all_r41/all_r31/all_r42
-    all_fobsarcsin = np.arcsin(np.sqrt(all_r31/all_r41))
-
-
-    lam_interp = np.complex128(np.linspace(np.min(all_lam), np.max(all_lam), nrho))
-    eta_interp = np.complex128(np.linspace(np.min(all_eta), np.max(all_eta), nrho))
-    up_interp = np.linspace(np.min(all_up), np.max(all_up), nrho)
-    um_interp = np.linspace(np.min(all_um), np.max(all_um), nrho)
-    k_interp = np.linspace(np.min(all_k), np.max(all_k), nrho)
-    fobsarcsin_interp = np.linspace(np.min(all_fobsarcsin), np.max(all_fobsarcsin))
-    um_interp-=1e-15
-    # return um_interp
-    ll, ee = np.meshgrid(lam_interp, eta_interp)
-
-    pp, mm = np.meshgrid(up_interp,um_interp)
-    urat = pp / mm
-    # plt.imshow(urat)
+def get_radius_interpolative(rho, varphi, inc, a, n, K_int, fobs_int, Fobs_int, sn_int_real, sn_int_imag):
+    """
+    Numerical: get rs from rho, varphi, inc, a, and subimage index n, interpolatively.
+    """
+    alpha = rho*np.cos(varphi)
+    beta = rho*np.sin(varphi)
+    lam, eta = get_lam_eta(alpha,beta, inc, a)
+    lam = np.complex128(lam)
+    eta = np.complex128(eta)
+    up, um = get_up_um(lam, eta, a)
+    # up[up/um > 1] = np.nan
+    # um[up/um > 1] = np.nan
+    # plt.imshow(up/um)
+    # plt.imshow(np.real(up/um))
     # plt.colorbar()
     # plt.show()
-    min_urat = np.min(urat)
-    max_urat = np.max(urat)
-    urat_interp = np.linspace(min_urat,max_urat, nrho)
-    return urat_interp
-    
-    K_exact = ef(np.pi/2, urat_interp)
-    K_interp = interp1d(urat_interp, K_exact)
+    r1, r2, r3, r4 = get_radroots(lam, eta, a)
+    r31 = r3-r1
+    r32 = r3-r2
+    r42 = r4-r2
+    r41 = r4-r1
+    k = r32*r41 / (r31*r42)
+    #build m array from beta sign and subimage index
+    m = np.sign(beta)
+    m[m<0] = 0
+    m += n
+    fobs = np.complex128(fobs_int(np.arcsin(np.sqrt(r31/r41)), k))
+    Fobs = np.complex128(Fobs_int(np.arcsin(np.cos(inc)/np.sqrt(up)), up/um))
+    plt.imshow(np.imag(fobs).reshape((40,40)))
+    plt.colorbar()
+    plt.show()
+    Ir = np.real(1/np.sqrt(-um*a**2)*(2*m*np.complex128(K_int(up/um)) - np.sign(beta)*Fobs))
+    # plt.imshow(np.real(Fobs).reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
+    ffac = 1 / 2 * (r31 * r42)**(1/2)
+    # plt.imshow(np.angle(ffac*Ir-fobs).reshape((40,40)))
+    # plt.title("Angle(ffac*Ir-fobs)")
+    # plt.colorbar()
+    # # plt.savefig('angle(ffrac*Ir-fobs).png')
+    # plt.show()
+    # plt.imshow(np.angle(k).reshape((40,40)))
+    # plt.title('Angle(k)')
+    # plt.colorbar()
+    # plt.show()
+    sn_real = sn_int_real(ffac*Ir - fobs, k)
+    sn_imag = sn_int_imag(ffac*Ir - fobs, k)
+    # plt.imshow(sn_imag.reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
+    # plt.imshow(np.imag(ffac*Ir-fobs).reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
+    # plt.imshow(np.imag(k).reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
+    sn_comp = sn_real+sn_imag*1j
+    snsqr = sn_comp**2
+    # plt.imshow(np.real(snsqr).reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
+    rs = np.real((r4*r31 - r3*r41*snsqr) / (r31-r41*snsqr))
+    return rs
+
+def build_sn_interpolator(ffacIr_fobs_diff, k):
+    ff, kk = np.meshgrid(ffacIr_fobs_diff, k)
+    sn_exact = np.complex128(sn('sn',ff, kk))
+    sn_real = np.real(sn_exact)
+    sn_imag = np.imag(sn_exact)
+    sn_int_base_real = interp2d(ffacIr_fobs_diff, k, sn_real)#, bounds_error=False, fill_value=0)
+    sn_int_real = lambda x, y: si.dfitpack.bispeu(sn_int_base_real.tck[0], sn_int_base_real.tck[1], sn_int_base_real.tck[2], sn_int_base_real.tck[3], sn_int_base_real.tck[4], x, y)[0]
+    sn_int_base_imag = interp2d(ffacIr_fobs_diff, k, sn_imag)
+    sn_int_imag = lambda x, y: si.dfitpack.bispeu(sn_int_base_imag.tck[0], sn_int_base_imag.tck[1], sn_int_base_imag.tck[2], sn_int_base_imag.tck[3], sn_int_base_imag.tck[4], x, y)[0]
+    return sn_int_real, sn_int_imag
+
+def build_Fobs_interpolator(Fobsangle, urat):
+    FF, uu = np.meshgrid(Fobsangle, urat)
+    Fobs = np.real(np.complex128(ef(FF, uu)))
+    Fobs_int_base = interp2d(Fobsangle, urat, Fobs)#, bounds_error=False, fill_value=0)
+    Fobs_int = lambda x, y: si.dfitpack.bispeu(Fobs_int_base.tck[0], Fobs_int_base.tck[1], Fobs_int_base.tck[2], Fobs_int_base.tck[3], Fobs_int_base.tck[4], x, y)[0]
+    return Fobs_int
+
+def build_fobs_interpolator(fobsangle, k):
+    ff, kk = np.meshgrid(fobsangle, k)
+    fobs = np.real(np.complex128(ef(ff, kk)))    
+    fobs_int_base = interp2d(fobsangle, k, fobs)#, bounds_error=False, fill_value=0)
+    fobs_int = lambda x, y: si.dfitpack.bispeu(fobs_int_base.tck[0], fobs_int_base.tck[1], fobs_int_base.tck[2], fobs_int_base.tck[3], fobs_int_base.tck[4], x, y)[0]
+    return fobs_int
+
+def build_K_interpolator(urat):
+    K = np.complex128(ef(np.pi/2, urat))
+    return interp1d(urat, K)
 
 
-    # ff, kk =     
-
-
-    fobs = np.complex128(ef(np.arcsin(np.sqrt(r31/r41)), k))
-    Fobs = np.complex128(ef(np.arcsin(np.cos(inc)/np.sqrt(up)), up/um))
-
+k = np.linspace(-1,1)
+fobsangle = np.linspace(0, np.pi/2)
+fobs_int = build_fobs_interpolator(fobsangle, k)
 
 
 
-    # r1, r2, r3, r4 = get_radroots(lam, eta, a)
-    # r31 = r3-r1
-    # r32 = r3-r2
-    # r42 = r4-r2
-    # r41 = r4-r1
-    
+urat = np.linspace(-10,10,100)
+Fobsangle = np.linspace(0, np.pi/2)
+Fobs_int = build_Fobs_interpolator(Fobsangle, urat)
 
+K_int = build_K_interpolator(urat)
 
-    return urat_interp
-
-
+ffacIr_fobs_diff = np.linspace(-5,10)
+sn_int_real, sn_int_imag = build_sn_interpolator(ffacIr_fobs_diff, k)
 
 
 
+# rho_interp = np.linspace(1e-3, 30,100)
+
+# out = build_all_interpolators(rho_interp)
 
 
-rho_interp = np.linspace(1e-3, 30,100)
+npix = 40
+pxi = (np.arange(npix)-0.01)/npix-0.5
+pxj = np.arange(npix)/npix-0.5
+# get angles measured north of west
+PXI,PXJ = np.meshgrid(pxi,pxj)
+varphi = np.arctan2(-PXJ,PXI)+1e-15# - np.pi/2
+# self.varphivec = varphi.flatten()
 
-out = build_all_interpolators(rho_interp)
-
-
-# npix = 40
-# pxi = (np.arange(npix)-0.01)/npix-0.5
-# pxj = np.arange(npix)/npix-0.5
-# # get angles measured north of west
-# PXI,PXJ = np.meshgrid(pxi,pxj)
-# varphi = np.arctan2(-PXJ,PXI)+1e-15# - np.pi/2
-# # self.varphivec = varphi.flatten()
-
-# #get grid of angular radii
-# fov = 11
-# mui = pxi*fov
-# muj = pxj*fov
-# MUI,MUJ = np.meshgrid(mui,muj)
-# rho = np.sqrt(np.power(MUI,2.)+np.power(MUJ,2.))
+#get grid of angular radii
+fov = 16
+mui = pxi*fov
+muj = pxj*fov
+MUI,MUJ = np.meshgrid(mui,muj)
+rho = np.sqrt(np.power(MUI,2.)+np.power(MUJ,2.))
 
 
-# inc = 17/180*np.pi
-# a = 0.01
-# n = 1
-# r = get_radius_exact(rho, varphi, inc, a, n)
+inc = 17/180*np.pi
+a = 0.99
+n = 0
+rhovec = rho.flatten()
+varphivec = varphi.flatten()
+r_interped = get_radius_interpolative(rhovec, varphivec, inc, a, n, K_int, fobs_int, Fobs_int, sn_int_real, sn_int_imag)
 # r[r<0] = 0
-# plt.imshow(r)
+# plt.imshow(r_interped.reshape((npix,npix)),extent=[-fov//2, fov//2, -fov//2, fov//2])
 # plt.colorbar()
 # plt.show()
 
 
-# a = 0.99
-# inc = 60/180*np.pi
-# n = 1
-# r = get_radius_exact(rho, varphi, inc, a, n)
+a = 0.99
+inc = 17/180*np.pi
+n = 0
+r = get_radius_exact(rhovec, varphivec, inc, a, n)
+# r[r==np.max(r)] = 0
+# plt.imshow(r.reshape((npix,npix)),extent=[-fov//2, fov//2, -fov//2, fov//2])
+# plt.colorbar()
+# plt.show()
+
+
+
+# plt.imshow(r.reshape((npix,npix))-r_interped.reshape((npix,npix)),extent=[-fov//2, fov//2, -fov//2, fov//2])
+# plt.colorbar()
+# plt.show()
+
+
+
+
 # r[r<0] = 0
 # r[r>2*np.max(rho)] = 2*np.max(rho)
 # plt.imshow(r)

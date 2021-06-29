e0906f71b0ea0e00f8c9add8f63a4ad5ee8f2a7c
danielpalumbo <daniel.palumbo@cfa.harvard.edu>
2021-06-29 18:46:10 -0400

    WIP commit, no idea why

--- bam/inference/kerrexact.py
+++ bam/inference/kerrexact.py
@@ -47,14 +47,55 @@ def get_radroots(lam, eta, a):
     C = -a**2 * eta
     P = - A**2 / 12 - C
     Q = -A/3 * ((A/6)**2 - C)-B**2/8
-    H = -Q + np.sqrt(12*P**3 + 81*Q**2)
+    H = -9*Q + np.sqrt(12*P**3 + 81*Q**2)
     z = np.sqrt((-2*(3**(1/3) * P) + 2**(1/3)*H**(2/3))/(2*6**(2/3)*H**(1/3))-A/6)
-    # z = np.abs(z)
     r1 = -z - np.sqrt(-A/2 - z**2 + B/(4*z))
     r2 = -z + np.sqrt(-A/2 - z**2 + B/(4*z))
     r3 = z - np.sqrt(-A/2 - z**2 - B/(4*z))
     r4 = z + np.sqrt(-A/2 - z**2 - B/(4*z))
     return r1,r2,r3,r4
+
+
+
+#get radial roots
+def getlametatest(alpha, beta, spin, theta0, lam1=0, eta1=0, uselameta=False):
+    lam = -alpha * np.sin(theta0)# * (not uselameta) + lam1 * uselameta
+    eta0 = (beta**2 + (alpha**2 - spin**2) * (np.cos(theta0))**2)# * (not uselameta) + eta1 * uselameta
+    return lam, eta0
+
+#get radial roots
+def getrootsrad(lam, eta, spin):#, spin, theta0, lam1=0, eta1=0, uselameta=False):
+    # lam = -alpha * np.sin(theta0) * (not uselameta) + lam1 * uselameta
+    # eta0 = (beta**2 + (alpha**2 - spin**2) * (np.cos(theta0))**2) * (not uselameta) + eta1 * uselameta
+    Aconst  = spin**2-eta-lam**2 + 0j
+    # print(Aconst)
+    Bconst = 2*(eta + (lam - spin)**2)
+    # print(Bconst)
+    Cconst = -spin**2 * eta
+    # print(Cconst)
+    Pconst = -1 * Aconst**2 / 12 - Cconst
+    print(Pconst)
+    Qconst = -Aconst / 3 * (Aconst**2 / 36 - Cconst) - Bconst**2 / 8
+    print(Qconst)
+    discr = -9 * Qconst + np.sqrt(12 * Pconst**3 + 81 * Qconst**2 + 0j)
+
+    #root to resolvent cubic using version of Cardano's method consistent with Mathematica (and python)
+    xi = (-2 * 3**(1/3) * Pconst + 2**(1/3) * discr**(2/3)) / (6**(2/3) * discr**(1/3)) - Aconst / 3
+
+    zconst = np.sqrt(xi / 2+0j)
+    print(zconst)
+    rootfac1 = -1 * Aconst / 2 - zconst**2 + Bconst / 4 / zconst
+    rootfac2 = -1 * Aconst / 2 - zconst**2 - Bconst / 4 / zconst
+    finalconst1 = np.sqrt(np.real(rootfac1)+(1j)*np.imag(rootfac1))
+    finalconst2 = np.sqrt(np.real(rootfac2)+(1j)*np.imag(rootfac2))
+    
+    r1 = -1 * zconst - finalconst1
+    r2 = -1 * zconst + finalconst1
+    r3 = zconst - finalconst2
+    r4 = zconst + finalconst2
+
+    return np.array([r1, r2, r3, r4])
+
 
 def get_radius_exact(rho, varphi, inc, a, n):
     """
@@ -66,13 +107,14 @@ def get_radius_exact(rho, varphi, inc, a, n):
     lam = np.complex128(lam)
     eta = np.complex128(eta)
     up, um = get_up_um(lam, eta, a)
-    # up[up/um > 1] = np.nan
-    # um[up/um > 1] = np.nan
-    # plt.imshow(up/um)
-    # plt.imshow(np.real(up/um))
-    # plt.colorbar()
-    # plt.show()
     r1, r2, r3, r4 = get_radroots(lam, eta, a)
+    plt.imshow(np.real(r1).reshape((40,40)))
+    plt.colorbar()
+    plt.show()
+    plt.imshow(np.real(r2).reshape((40,40)))
+    plt.colorbar()
+    plt.show()
+    
     r31 = r3-r1
     r32 = r3-r2
     r42 = r4-r2
@@ -82,41 +124,21 @@ def get_radius_exact(rho, varphi, inc, a, n):
     m = np.sign(beta)
     m[m<0] = 0
     m += n
-    print(np.arcsin(np.sqrt(r31/r41)))
+    # plt.imshow(np.angle(np.arcsin(np.sqrt(r31/r41))).reshape(40,40))
+    # plt.colorbar()
+    # plt.show()
+    # print(np.arcsin(np.sqrt(r31/r41)))
     fobs = np.complex128(ef(np.arcsin(np.sqrt(r31/r41)), k))
-    plt.imshow(np.imag(fobs).reshape((40,40)))
-    plt.colorbar()
-    plt.show()
+    # plt.imshow(np.abs(fobs).reshape((40,40)))
+    # plt.colorbar()
+    # plt.show()
     Fobs = np.complex128(ef(np.arcsin(np.cos(inc)/np.sqrt(up)), up/um))
     Ir = np.real(1/np.sqrt(-um*a**2)*(2*m*np.complex128(ef(np.pi/2, up/um)) - np.sign(beta)*Fobs))
-    
+    # plt.imshow(Ir.reshape((40,40)))
+    # plt.show()
     ffac = 1 / 2 * (r31 * r42)**(1/2)
-    # plt.imshow(np.real(ffac*Ir-fobs))
-    # plt.colorbar()
-    # plt.show()
     snnum = np.complex128(sn('sn',ffac*Ir-fobs,k))
-
-    # plt.imshow(np.angle(ffac*Ir-fobs).reshape((40,40)))
-    # plt.title("Angle(ffac*Ir-fobs)")
-    # plt.colorbar()
-    # plt.show()
-    # plt.imshow(np.angle(k).reshape((40,40)))
-    # plt.title('Angle(k)')
-    # plt.colorbar()
-    # plt.show()
-    # plt.imshow(np.imag(snnum).reshape((40,40)))
-    # plt.colorbar()
-    # plt.show()
-    # plt.imshow(np.imag(ffac*Ir-fobs).reshape((40,40)))
-    # plt.colorbar()
-    # plt.show()
-    # plt.imshow(np.imag(k).reshape((40,40)))
-    # plt.colorbar()
-    # plt.show()
-    snsqr = np.complex128(sn('sn', ffac * Ir - fobs, k))**2
-    # plt.imshow(np.real(snsqr).reshape((40,40)))
-    # plt.colorbar()
-    # plt.show()
+    snsqr = snnum**2
     rs = np.real((r4*r31 - r3*r41*snsqr) / (r31-r41*snsqr))
     return rs
 
@@ -130,13 +152,8 @@ def get_radius_interpolative(rho, varphi, inc, a, n, K_int, fobs_int, Fobs_int, 
     lam = np.complex128(lam)
     eta = np.complex128(eta)
     up, um = get_up_um(lam, eta, a)
-    # up[up/um > 1] = np.nan
-    # um[up/um > 1] = np.nan
-    # plt.imshow(up/um)
-    # plt.imshow(np.real(up/um))
-    # plt.colorbar()
-    # plt.show()
     r1, r2, r3, r4 = get_radroots(lam, eta, a)
+
     r31 = r3-r1
     r32 = r3-r2
     r42 = r4-r2
@@ -148,39 +165,12 @@ def get_radius_interpolative(rho, varphi, inc, a, n, K_int, fobs_int, Fobs_int, 
     m += n
     fobs = np.complex128(fobs_int(np.arcsin(np.sqrt(r31/r41)), k))
     Fobs = np.complex128(Fobs_int(np.arcsin(np.cos(inc)/np.sqrt(up)), up/um))
-    plt.imshow(np.imag(fobs).reshape((40,40)))
-    plt.colorbar()
-    plt.show()
     Ir = np.real(1/np.sqrt(-um*a**2)*(2*m*np.complex128(K_int(up/um)) - np.sign(beta)*Fobs))
-    # plt.imshow(np.real(Fobs).reshape((40,40)))
-    # plt.colorbar()
-    # plt.show()
     ffac = 1 / 2 * (r31 * r42)**(1/2)
-    # plt.imshow(np.angle(ffac*Ir-fobs).reshape((40,40)))
-    # plt.title("Angle(ffac*Ir-fobs)")
-    # plt.colorbar()
-    # # plt.savefig('angle(ffrac*Ir-fobs).png')
-    # plt.show()
-    # plt.imshow(np.angle(k).reshape((40,40)))
-    # plt.title('Angle(k)')
-    # plt.colorbar()
-    # plt.show()
     sn_real = sn_int_real(ffac*Ir - fobs, k)
     sn_imag = sn_int_imag(ffac*Ir - fobs, k)
-    # plt.imshow(sn_imag.reshape((40,40)))
-    # plt.colorbar()
-    # plt.show()
-    # plt.imshow(np.imag(ffac*Ir-fobs).reshape((40,40)))
-    # plt.colorbar()
-    # plt.show()
-    # plt.imshow(np.imag(k).reshape((40,40)))
-    # plt.colorbar()
-    # plt.show()
     sn_comp = sn_real+sn_imag*1j
     snsqr = sn_comp**2
-    # plt.imshow(np.real(snsqr).reshape((40,40)))
-    # plt.colorbar()
-    # plt.show()
     rs = np.real((r4*r31 - r3*r41*snsqr) / (r31-r41*snsqr))
     return rs
 
@@ -214,20 +204,24 @@ def build_K_interpolator(urat):
     return interp1d(urat, K)
 
 
-k = np.linspace(-1,1)
-fobsangle = np.linspace(0, np.pi/2)
-fobs_int = build_fobs_interpolator(fobsangle, k)
+# print(np.real(r2))
+
+# l, e = getlametatest(alpha,beta,a,inc)
+
+# k = np.linspace(-1,1)
+# fobsangle = np.linspace(0, np.pi/2)
+# fobs_int = build_fobs_interpolator(fobsangle, k)
 
 
 
-urat = np.linspace(-10,10,100)
-Fobsangle = np.linspace(0, np.pi/2)
-Fobs_int = build_Fobs_interpolator(Fobsangle, urat)
+# urat = np.linspace(-10,10,100)
+# Fobsangle = np.linspace(0, np.pi/2)
+# Fobs_int = build_Fobs_interpolator(Fobsangle, urat)
 
-K_int = build_K_interpolator(urat)
+# K_int = build_K_interpolator(urat)
 
-ffacIr_fobs_diff = np.linspace(-5,10)
-sn_int_real, sn_int_imag = build_sn_interpolator(ffacIr_fobs_diff, k)
+# ffacIr_fobs_diff = np.linspace(-5,10)
+# sn_int_real, sn_int_imag = build_sn_interpolator(ffacIr_fobs_diff, k)
 
 
 
@@ -257,7 +251,7 @@ a = 0.99
 n = 0
 rhovec = rho.flatten()
 varphivec = varphi.flatten()
-r_interped = get_radius_interpolative(rhovec, varphivec, inc, a, n, K_int, fobs_int, Fobs_int, sn_int_real, sn_int_imag)
+# r_interped = get_radius_interpolative(rhovec, varphivec, inc, a, n, K_int, fobs_int, Fobs_int, sn_int_real, sn_int_imag)
 # r[r<0] = 0
 # plt.imshow(r_interped.reshape((npix,npix)),extent=[-fov//2, fov//2, -fov//2, fov//2])
 # plt.colorbar()
@@ -268,10 +262,10 @@ a = 0.99
 inc = 17/180*np.pi
 n = 0
 r = get_radius_exact(rhovec, varphivec, inc, a, n)
-# r[r==np.max(r)] = 0
-# plt.imshow(r.reshape((npix,npix)),extent=[-fov//2, fov//2, -fov//2, fov//2])
-# plt.colorbar()
-# plt.show()
+r[r==np.max(r)] = 0
+plt.imshow(r.reshape((npix,npix)),extent=[-fov//2, fov//2, -fov//2, fov//2])
+plt.colorbar()
+plt.show()
 
 
 
